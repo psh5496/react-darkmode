fe08f77bea01cfa53af50678c2b38fadee888a0b
psh549633 <psh549633@gmail.com>
2022-08-04 17:30:49 +0900

    :lipstick: :: 테마 지정하기

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  bgColor: "#f8f7f4",
+  textColor: "#31302E",
+};
+
+export const darkTheme = {
+  bgColor: "#1e1e22",
+  textColor: "#ccc",
+};
+
+export type ThemeType = typeof lightTheme;
