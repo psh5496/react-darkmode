40ec286683ff0b663c86c990317e1d347fe1e476
psh549633 <psh549633@gmail.com>
2022-08-04 17:33:16 +0900

    :hammer: :: context, theme 설정 및 요소 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { createContext } from "react";
+import { Global, ThemeProvider } from "@emotion/react";
+import ToggleSwitch from "./components/toggleSwitch";
+import { useDarkMode } from "./hooks/useDarkMode";
+import { lightTheme, ThemeType } from "./styles/theme";
+import GlobalStyle from "./styles/globalStyle";
+
+interface ContextProps {
+  theme: ThemeType;
+  toggleMode: () => void;
+}
+
+export const ThemeContext = createContext<ContextProps>({
+  theme: lightTheme,
+  toggleMode: () => {
+    return null;
+  },
+});
+
 const App = () => {
+  const { theme, toggleMode } = useDarkMode();
+
   return (
-    <div>
-      <div />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleMode }}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <ToggleSwitch />
+        <h1>안녕하세요</h1>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 };
 
